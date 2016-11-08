9f1d029587cb6cf6f9c2bc717a29461106a1d425
Flavio Alves da Costa <flavioalves996@gmail.com>
2016-11-08 16:00:28 -0200

    [BackEnd] Flavio Banco aluno

--- src/br/edu/fametro/portal/listener/Persistencia.java
+++ src/br/edu/fametro/portal/listener/Persistencia.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import br.edu.fametro.portal.business.AlunoBusiness;
 import br.edu.fametro.portal.business.SecretarioBusiness;
 import br.edu.fametro.portal.business.enums.AvaliacaoBusiness;
 import br.edu.fametro.portal.business.enums.CursoBusiness;
@@ -16,7 +17,9 @@ import br.edu.fametro.portal.model.DateUtility;
 import br.edu.fametro.portal.model.Endereco;
 import br.edu.fametro.portal.model.Filiacao;
 import br.edu.fametro.portal.model.Telefone;
+import br.edu.fametro.portal.model.atores.Aluno;
 import br.edu.fametro.portal.model.atores.Secretario;
+import br.edu.fametro.portal.model.enums.Curso;
 import br.edu.fametro.portal.model.enums.Genero;
 
 /**
@@ -104,6 +107,63 @@ public class Persistencia implements ServletContextListener {
 	
 		System.out.println("[SECRETARIO] Colocando banco no contexto da aplicação...\n");
 		contexto.getServletContext().setAttribute("bancoSecretario", bancoSecretario);
+		
+		/****************************************************************************************************/
+		System.out.println("[ALUNO] Instanciando banco...");
+		AlunoBusiness bancoAluno = new AlunoBusiness();
+
+		System.out.println("[ALUNO] Criando o primeiro elemento...");
+		Aluno alunoExemplo = new Aluno(bancoAluno.getSize());
+		// Pessoa
+		alunoExemplo.setNome("Aluno Fulano Beltrano");
+		alunoExemplo.setRg("5368374810");
+		alunoExemplo.setCpf("123.456.789-00");
+		alunoExemplo.setNascimento(DateUtility.maskToDate("01/02/1993"));
+		alunoExemplo.setGenero(Genero.MASCULINO);
+		{
+			Filiacao filiacao = new Filiacao();
+			filiacao.setPai("Pai do Aluno Beltrano");
+			filiacao.setMae("Mãe do Aluno Beltrano");
+			
+			alunoExemplo.setFiliacao(filiacao);
+		}
+		alunoExemplo.setNaturalidade("Fortaleza");
+		alunoExemplo.setEstadoNatal("CE");
+		// Aluno
+		alunoExemplo.setCurso(Curso.ADS);
+		{
+			Endereco endereco = new Endereco();
+			endereco.setCep("60750-000");
+			endereco.setLogradouro("Avenida Araquém Aguiar");
+			endereco.setComplemento("Complemento");
+			endereco.setNumero(234);
+			endereco.setBairro("Prefeito José Walter");
+			endereco.setEstado("CE");
+			endereco.setCidade("Fortaleza");
+			endereco.setPais("Brasil");
+
+			alunoExemplo.setEndereco(endereco);
+		}
+		alunoExemplo.setResidencial(Telefone.maskToTelefone("(85)03473-7070"));
+		alunoExemplo.setCelular(Telefone.maskToTelefone("(85)97070-7070"));
+		alunoExemplo.setOpcional(Telefone.maskToTelefone("(85)99876-5432"));
+
+		System.out.println("[ALUNO] Colocando o primeiro elemento no banco...");
+		bancoAluno.adicionar(alunoExemplo);
+
+		// Teste
+		System.out.println("[ALUNO] Quantidade de elementos no banco: " + bancoAluno.getSize());
+		{
+			int i = 0;
+			for (Aluno s : bancoAluno.getBanco()) {
+				System.out.print("[ALUNO] Elemento " + i +": " + s +"\n");
+				i++;
+			}
+		}
+		// Fim Teste
+	
+		System.out.println("[ALUNO] Colocando banco no contexto da aplicação...\n");
+		contexto.getServletContext().setAttribute("bancoAluno", bancoAluno);
 
 		/****************************************************************************************************/
 		System.out.println("[ENUM] Instanciando banco...");
