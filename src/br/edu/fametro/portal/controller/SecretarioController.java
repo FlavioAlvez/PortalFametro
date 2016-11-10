abdf4569a0e7030c8760c0f808cb5bc17f752ec0
Flavio Alves da Costa <flavioalves996@gmail.com>
2016-11-10 01:33:50 -0200

    [BackEnd] Douglas SecretarioController

--- src/br/edu/fametro/portal/controller/SecretarioController.java
+++ src/br/edu/fametro/portal/controller/SecretarioController.java
@@ -13,9 +13,10 @@ import br.edu.fametro.portal.business.SecretarioBusiness;
 import br.edu.fametro.portal.business.enums.GeneroBusiness;
 import br.edu.fametro.portal.model.DateUtility;
 import br.edu.fametro.portal.model.Endereco;
-
+import br.edu.fametro.portal.model.Filiacao;
 import br.edu.fametro.portal.model.Telefone;
 import br.edu.fametro.portal.model.atores.Secretario;
+import br.edu.fametro.portal.model.atores.Usuario;
 import br.edu.fametro.portal.model.enums.Genero;
 
 /**
@@ -49,25 +50,34 @@ public class SecretarioController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Qual método sera escolhido na tela de cadastro do secretário
 		try {
 			String action = request.getParameter("action");
 
 			switch (action.toLowerCase()) {
 			case "cadastrar":
-				cadastrarSecretario(request, response);
+				cadastro(request, response);
+				break;
+			case "alterar perfil":
+				alterarPerfil(request, response);
+				break;
+			case "alterar senha":
+				alterarSenha(request, response);
+				break;
+			default:
+				System.err.println("[SecretárioController] action entrou no default!");
+				System.err.println("[SecretárioController] action = " + (action == null ? "null" : action));
+				response.sendRedirect("LoginController.do");
 				break;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.sendRedirect("LoginController.do");
 		}
-
 	}
 
-	private void cadastrarSecretario(HttpServletRequest request, HttpServletResponse response)
+	private void cadastro(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Dados da Identificacao
+		// Identificação
 		String nome = request.getParameter("nome");
 		String rg = request.getParameter("rg");
 		String cpf = request.getParameter("cpf");
@@ -76,28 +86,28 @@ public class SecretarioController extends HttpServlet {
 		String naturalidade = request.getParameter("naturalidade");
 		String estadoNatal = request.getParameter("estado-natal");
 
-		// Filiacao
+		// Filiação
 		String pai = request.getParameter("pai");
 		String mae = request.getParameter("mae");
 
-		// Endereco
+		// Endereço
 		String cep = request.getParameter("cep");
 		String logradouro = request.getParameter("rua");
 		String complemento = request.getParameter("complemento");
 		String numero = request.getParameter("numero");
 		String bairro = request.getParameter("bairro");
-		String estado = request.getParameter("uf");
+		String estado = request.getParameter("estado");
 		String cidade = request.getParameter("cidade");
 		String pais = request.getParameter("pais");
 
 		// Contato
 		String email = request.getParameter("email");
-		String foneResidencia = request.getParameter("fone-residencial");
-		String foneCelular = request.getParameter("fone-celular");
-		String foneOpcional = request.getParameter("fone-3");
+		String residencial = request.getParameter("fone-residencial"); // (12)12345-6789
+		String celular = request.getParameter("fone-celular");
+		String opcional = request.getParameter("fone-3");
 
 		// TESTE
-		System.out.println("----- IDENTIFICAO -----");
+		System.out.println("----- IDENTIFICAÇÃO -----");
 		System.out.println("Nome: " + nome);
 		System.out.println("RG: " + rg);
 		System.out.println("CPF: " + cpf);
@@ -106,9 +116,9 @@ public class SecretarioController extends HttpServlet {
 		System.out.println("Naturalidade: " + naturalidade);
 		System.out.println("Estado Natal: " + estadoNatal);
 		System.out.println();
-		System.out.println("------- FILIAÇAO --------");
+		System.out.println("------- FILIAÇÃO --------");
 		System.out.println("Nome do Pai: " + pai);
-		System.out.println("Nome da Mae: " + mae);
+		System.out.println("Nome da Mãe: " + mae);
 		System.out.println();
 		System.out.println("------- ENDEREÇO --------");
 		System.out.println("CEP: " + cep);
@@ -122,19 +132,18 @@ public class SecretarioController extends HttpServlet {
 		System.out.println();
 		System.out.println("-------- CONTATO --------");
 		System.out.println("Email: " + email);
-		System.out.println("Telefone Residencial: " + foneResidencia);
-		System.out.println("Telefone Celular: " + foneCelular);
-		System.out.println("Telefone 3: " + foneOpcional);
+		System.out.println("Telefone Residencial: " + residencial);
+		System.out.println("Telefone Celular: " + celular);
+		System.out.println("Telefone 3: " + opcional);
 		System.out.println();
 
-		// Criando uma cópia local do banco
+		// Resgatando o banco
 		SecretarioBusiness bancoSecretario = (SecretarioBusiness) request.getServletContext()
 				.getAttribute("bancoSecretario");
 
-		// Criar objeto instanciando
+		// Criar objeto - Instanciando
 		Secretario secretario = new Secretario(bancoSecretario.getSize());
-
-		// Setar informações de Identificação
+		// Criar objeto - Identificação
 		secretario.setNome(nome);
 		secretario.setRg(rg);
 		secretario.setCpf(cpf);
@@ -146,14 +155,19 @@ public class SecretarioController extends HttpServlet {
 		}
 		{
 			Date dtNascimento = DateUtility.maskToDate(nascimento);
+
 			secretario.setNascimento(dtNascimento);
 		}
-
 		secretario.setNaturalidade(naturalidade);
 		secretario.setEstadoNatal(estadoNatal);
-
-		secretario.getFiliacao().setPai(pai);
-		secretario.getFiliacao().setMae(mae);
+		// Criar objeto - Filiação
+		{
+			Filiacao filiacao = new Filiacao();
+			filiacao.setPai(pai);
+			filiacao.setMae(mae);
+			secretario.setFiliacao(filiacao);
+		}
+		// Criar objeto - Endereço
 		{
 			Endereco endereco = new Endereco();
 			endereco.setCep(cep);
@@ -164,15 +178,14 @@ public class SecretarioController extends HttpServlet {
 			endereco.setEstado(estado);
 			endereco.setCidade(cidade);
 			endereco.setPais(pais);
+
 			secretario.setEndereco(endereco);
 		}
-		secretario.setEmail(email);
-
 		// Criar objeto - Contato
 		secretario.setEmail(email);
-		secretario.setResidencial(Telefone.maskToTelefone(foneResidencia));
-		secretario.setCelular(Telefone.maskToTelefone(foneCelular));
-		secretario.setOpcional(Telefone.maskToTelefone(foneOpcional));
+		secretario.setResidencial(Telefone.maskToTelefone(residencial));
+		secretario.setCelular(Telefone.maskToTelefone(celular));
+		secretario.setOpcional(Telefone.maskToTelefone(opcional));
 
 		// Adicionando o objeto ao banco
 		boolean adicionado = bancoSecretario.adicionar(secretario);
@@ -192,4 +205,153 @@ public class SecretarioController extends HttpServlet {
 		}
 	}
 
+	public void alterarPerfil(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Identificação
+		String matricula = request.getParameter("registro");
+		String nome = request.getParameter("nome");
+		String nascimento = request.getParameter("data-nascimento");
+		String naturalidade = request.getParameter("naturalidade");
+		String estadoNatal = request.getParameter("estado-natal");
+
+		// Endereço
+		String cep = request.getParameter("cep");
+		String logradouro = request.getParameter("logradouro");
+		String complemento = request.getParameter("complemento");
+		String numero = request.getParameter("numero");
+		String bairro = request.getParameter("bairro");
+		String estado = request.getParameter("estado");
+		String cidade = request.getParameter("cidade");
+		String pais = request.getParameter("pais");
+
+		// Contato
+		String email = request.getParameter("email");
+		String residencial = request.getParameter("fone-residencial"); // (12)12345-6789
+		String celular = request.getParameter("fone-celular");
+		String opcional = request.getParameter("fone-3");
+
+		// TESTE
+		System.out.println("----- IDENTIFICAÇÃO -----");
+		System.out.println("Registro Acadêmico: " + matricula);
+		System.out.println("Nome: " + nome);
+		System.out.println("Data de Nascimento: " + nascimento);
+		System.out.println("Naturalidade: " + naturalidade);
+		System.out.println("Estado Natal: " + estadoNatal);
+		System.out.println();
+		System.out.println("------- ENDEREÇO --------");
+		System.out.println("CEP: " + cep);
+		System.out.println("Logradouro: " + logradouro);
+		System.out.println("Complemento: " + complemento);
+		System.out.println("Numero: " + numero);
+		System.out.println("Bairro: " + bairro);
+		System.out.println("Estado: " + estado);
+		System.out.println("Cidade: " + cidade);
+		System.out.println("País: " + pais);
+		System.out.println();
+		System.out.println("-------- CONTATO --------");
+		System.out.println("Email: " + email);
+		System.out.println("Telefone Residencial: " + residencial);
+		System.out.println("Telefone Celular: " + celular);
+		System.out.println("Telefone 3: " + opcional);
+		System.out.println();
+
+		// Resgatando o banco
+		SecretarioBusiness bancoSecretario = (SecretarioBusiness) request.getServletContext()
+				.getAttribute("bancoSecretario");
+
+		// Objeto à ser alterado
+		Secretario secretario = (Secretario) request.getSession().getAttribute("usuarioLogado");
+
+		// Alterar os dados no objeto local
+		{
+			Endereco endereco = new Endereco();
+			endereco.setCep(cep);
+			endereco.setLogradouro(logradouro);
+			endereco.setComplemento(complemento);
+			endereco.setNumero(Integer.parseInt(numero));
+			endereco.setBairro(bairro);
+			endereco.setEstado(estado);
+			endereco.setCidade(cidade);
+			endereco.setPais(pais);
+
+			secretario.setEndereco(endereco);
+		}
+		secretario.setEmail(email);
+		secretario.setResidencial(Telefone.maskToTelefone(residencial));
+		secretario.setCelular(Telefone.maskToTelefone(celular));
+		secretario.setOpcional(Telefone.maskToTelefone(opcional));
+
+		// Alterando o objeto no banco local
+		boolean alterado = bancoSecretario.alterar(secretario);
+
+		if (alterado) {
+			// Colocando o banco de volta ao escopo da aplicação
+			request.getServletContext().setAttribute("bancoSecretario", bancoSecretario);
+			request.getSession().setAttribute("usuarioLogado", secretario);
+			request.setAttribute("sucesso", Boolean.TRUE);
+			request.getRequestDispatcher("perfil.jsp").forward(request, response);
+		} else {
+			request.setAttribute("erro", Boolean.TRUE);
+			request.getRequestDispatcher("perfil.jsp").forward(request, response);
+		}
+	}
+
+	public void alterarSenha(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Identificação
+		String matricula = request.getParameter("registro");
+		// String nome = request.getParameter("nome");
+		// String nascimento = request.getParameter("data-nascimento");
+		// String naturalidade = request.getParameter("naturalidade");
+		// String estadoNatal = request.getParameter("estado-natal");
+
+		// Segurança
+		String senhaAtual = request.getParameter("senha-atual");
+		String novaSenha = request.getParameter("nova-senha");
+		String confirmarNovaSenha = request.getParameter("confirmar-senha");
+
+		// Resgatando o banco
+		SecretarioBusiness bancoSecretario = (SecretarioBusiness) request.getServletContext()
+				.getAttribute("bancoSecretario");
+
+		// Objeto à ser alterado
+		Secretario secretario = (Secretario) request.getSession().getAttribute("usuarioLogado");
+
+		// Criar Usuario com dados passados na senhaAtual
+		Usuario usuarioAtual = new Usuario(matricula, senhaAtual);
+
+		// Conferindo senha atual
+		boolean validado = false;
+
+		if (secretario.getUsuario().getSenha().equals(usuarioAtual.getSenha())) {
+			// Conferir se senhas novas são iguais
+			if (novaSenha.equals(confirmarNovaSenha)) {
+				validado = true;
+			}
+		}
+
+		if (validado) {
+			// Alterando o objeto
+			Usuario buffer = secretario.getUsuario();
+			buffer.setSenha(senhaAtual);
+			secretario.setUsuario(buffer);
+
+			// Alterando o objeto no banco local
+			boolean alterado = bancoSecretario.alterar(secretario);
+
+			if (alterado) {
+				// Colocando o banco de volta ao escopo da aplicação
+				request.getServletContext().setAttribute("bancoSecretario", bancoSecretario);
+				request.getSession().setAttribute("usuarioLogado", secretario);
+				request.setAttribute("sucesso", Boolean.TRUE);
+				request.getRequestDispatcher("alterar-senha.jsp").forward(request, response);
+			} else {
+				request.setAttribute("erro", Boolean.TRUE);
+				request.getRequestDispatcher("alterar-senha.jsp").forward(request, response);
+			}
+		} else {
+			request.setAttribute("erro", Boolean.TRUE);
+			request.getRequestDispatcher("alterar-senha.jsp").forward(request, response);
+		}
+	}
 }
