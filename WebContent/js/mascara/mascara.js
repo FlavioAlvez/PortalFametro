34c2ffbc653902e58947aa4289b398c7908f820b
Flavio Alves da Costa <flavioalves996@gmail.com>
2016-11-14 19:42:52 -0200

    [FrontEnd] Flavio Remoção de mascaras em campos tipo date

--- WebContent/js/mascara/mascara.js
+++ WebContent/js/mascara/mascara.js
@@ -1,9 +1,5 @@
 
-$(document).ready(function(){
-//		$("#data-nascimento").mask("99/99/9999");
-//		$("#data-abertura").mask("99/99/9999");
-//		$("#data-fechamento").mask("99/99/9999"); 
-//		$("#nascimento").mask("99/99/9999");		
+$(document).ready(function(){ 		
         $("#cep").mask("99999-999");
         $("#cpf").mask("999.999.999-99");
         $("#fone-residencial").mask("(99)9999-9999");
